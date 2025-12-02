0e81cd53ecc7b6215dd312c36e155ec233daf4f5
KostarevEgor1 <kostareffeg@yandex.ru>
2025-12-02 15:34:41 +0300

    Create test_report.py

---
+++ tests/test_report.py
@@ -0,0 +1,14 @@
+from src.report import generator
+
+
+def test_render_student_report():
+    html = generator.render_student_report(
+        student_id=101,
+        data={"activity": {"forum": 2}, "grades": {"final_grade": 85}},
+        predicted_grade=87.3,
+        recommendations=["Пишите в форум!"],
+        plot_html="<div>Mock Plot</div>"
+    )
+    assert "Студент ID: 101" in html
+    assert "87.3" in html
+    assert "Пишите в форум!" in html
